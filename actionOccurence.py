8bd2ad16affa1b5bfc8c3a5acedf9b5f78f31f54
IvanMatyazh <ivan070297@gmail.com>
2018-05-08 16:02:51 +0200

    Apply python convention to action occurence class

--- actionOccurence.py
+++
@@ -1,6 +0,0 @@
-class ActionOccurence:
-    def __init__(self, name, beginTime, duration):
-        self.Name = name
-        self.BeginTime = beginTime
-        self.Duration = duration
-
---
+++ action_occurence.py
@@ -0,0 +1,6 @@
+class ActionOccurence:
+    def __init__(self, name, begin_time, duration):
+        self.name = name
+        self.begin_time = begin_time
+        self.duration = duration
+
