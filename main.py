92b1288c510fcb9aba3ee2c33c1183224cb2e226
matrackif <filip.matracki@gmail.com>
2018-05-08 16:04:53 +0200

    Add argument parsing

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import argparse
 
 if __name__ == '__main__':
-    print('KRR')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--library-file', nargs='?', type=str, default='lib.adl3',
+                        const='lib.adl3', help='Path to input ADL3 file')
+    parser.add_argument('-s', '--scenario-file', nargs='?', type=str, default='scenario.adl3',
+                        const='scenario.adl3', help='Path to input ADL3 scenario file')
+    parser.add_argument('-q', '--query', nargs='?', type=str, help='Query string to be executed')
+    program_args = vars(parser.parse_args())
+    print('Program arguments:', program_args)
