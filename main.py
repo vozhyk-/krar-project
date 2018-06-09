e3cbe3f8fc261b6c90bde8878a14036e1bb5214b
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2018-06-09 15:32:59 +0200

    Rename parsing.scenario{_parser -> }

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import argparse
 from language_structure import LanguageStructure
 from structs.scenario import Scenario
 from structs.query import Query
-import parsing.scenario_parser
+import parsing.scenario
 from parsing.domain_description_parser import DomainDescriptionParser
 
 
 def main(library_file: str, scenario_file: str, query: str = None):
     structure = LanguageStructure(library_file)
     dd_parser = DomainDescriptionParser()
-    parsing.scenario_parser.parse(scenario_file)
+    parsing.scenario.parse(scenario_file)
     dd_parser.parse(library_file)
     raw_query = query
     if raw_query is None:
--- parsing/test_scenario.py
+++ parsing/test_scenario.py
@@ -1,12 +1,12 @@
 import unittest
 
-import parsing.scenario_parser
+import parsing.scenario
 from structs.condition import Condition
 
 
 class ScenarioParserTestCase(unittest.TestCase):
     def test_test(self):
-        scenario = parsing.scenario_parser.parse("example/scenario.txt")
+        scenario = parsing.scenario.parse("example/scenario.txt")
 
         assert len(scenario.observations) == 1
         assert scenario.observations[0].begin_time == 0
