f8168c7ac24b3d7167e95eedced609adafdee831
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2018-06-09 21:06:50 +0200

    Add parsing.test_condition

---
+++ parsing/test_condition.py
@@ -0,0 +1,16 @@
+import unittest
+import sympy
+
+import parsing.condition
+
+
+class ConditionParsingTestCase(unittest.TestCase):
+    def test_or(self):
+        condition = parsing.condition.parse("sick | dead")
+        sick, dead = sympy.symbols("sick,dead")
+        assert condition.formula == sick | dead
+
+    def test_and_not(self):
+        condition = parsing.condition.parse("~loaded & ~alive")
+        loaded, alive = sympy.symbols("loaded,alive")
+        assert condition.formula == ~loaded & ~alive
