c466144fbef176b91f0bfb5fc811bfee18a6d517
Ivan Matyazh <ivan070297@gmail.com>
2018-09-07 07:15:54 +0300

    Code cleaup: remove language_structure file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-from language_structure import LanguageStructure
 from structs.query import Query
 from engine.inconsistency_checker import InconsistencyChecker
 from engine.preprocessor import Preprocessor
@@ -12,7 +11,6 @@ import parsing.query
 
 
 def main(library_file: str, scenario_file: str, query_file: str = None):
-    #structure = LanguageStructure(library_file)  # What is this class for lol?
     scenario = parsing.scenario.parse_file(scenario_file)
     domain_desc = parsing.domain_description.parse_file(library_file)
     queries = parsing.query.parse_file(query_file)
